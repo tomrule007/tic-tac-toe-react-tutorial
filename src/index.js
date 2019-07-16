761d481d021372bd74c40f99c7752824f32830ca
Tom <Tomrule007@gmail.com>
2019-07-15 22:27:45 -0700

    testing two

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            // testing
+            // testing two
             <button className="square">
                 {this.props.value}
             </button>
